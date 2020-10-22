cc348c306f92b90e915ea64a7adc7464b5a2fc99
Barış Can Çifçi <50074715+bariscn@users.noreply.github.com>
2020-10-22 19:36:49 +0300

    Add files via upload

---
+++ Todo-List/app.js
@@ -0,0 +1,107 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
+//Functions
+function addTodo(event) {
+  //Prevent form from submitting
+  event.preventDefault();
+  //Create DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  saveLocalTodos(todoInput.value);
+  // CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-clipboard-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // DELETE MARK BUTTON
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  deleteButton.classList.add("delete-btn");
+  todoDiv.appendChild(deleteButton);
+
+  //APPEND TO LİST
+  todoList.appendChild(todoDiv);
+
+  //Clear Todo INPUT VALUE
+  todoInput.value = "";
+}
+function deleteCheck(e) {
+  const item = e.target;
+
+  if (item.classList[0] === "delete-btn") {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //CHECK MARK
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+    
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "block";
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "block";
+        } else {
+          todo.style.display = "none";
+        }
+        case "uncompleted":
+          if(!todo.classList.contains('completed')){
+            todo.style.display = "flex";
+          }
+          else {
+            todo.style.display = "none";
+          }
+           break;
+    }
+  });
+}
+
+function saveLocalTodos(todo){
+  let todos;
+  if(localStorage.getItem('todos')===null){
+    todos = [];
+  }else {
+    todos = JSON.parse(localStorage.getItem('toodos'));
+  }
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos() {
+  let todos;
+  if(localStorage.getItem('todos')===null){
+    todos = [];
+  }else {
+    todos = JSON.parse(localStorage.getItem('toodos'));
+  }
+  todos.forEach(function(todo))
+}
